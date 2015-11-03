14bf3a084e1bcf0310f8fc27cff4b89c72625bc5
Moses <none@none>
2015-11-03 00:20:24 -0500

    use of dict instead of list for 'seen' search

--- Dictionary Combine Method.py
+++ Dictionary Combine Method.py
@@ -7,25 +7,24 @@ def Combine(Main_Dict, *Dicts, **kwargs):
     will be handeled properly.
     usage = Combine(Main_Dict, *Dicts_with_common_elements)'''
     if "Seen" not in kwargs:
-        kwargs["Seen"] = list(Main_Dict)
-        kwargs["Seen"].extend(Dicts)
+        kwargs["Seen"] = d = {id(Main_Dict):Main_Dict}
+        for key in Dicts.keys():
+            d[id(Dicts[key])] = Dicts[key]
     if "Copy" not in kwargs:
         kwargs["Copy"] = "ref"
         
     for Dict in Dicts:
         for key in Dict:
-            
             #if the key already exists
             if key in Main_Dict:
-                
                 #if the entry in both the main dict and the common dict is
                 #a dict, then we merge entries from it into the main dict
                 if (isinstance(Dict[key], dict) and
                     isinstance(Main_Dict[key], dict) and
-                    Dict[key] not in kwargs["Seen"]):
+                    id(Dict[key]) not in kwargs["Seen"]):
                     
-                    kwargs["Seen"].append(Main_Dict[key])
-                    kwargs["Seen"].append(Dict[key])
+                    kwargs["Seen"][id(Main_Dict[key])] = Main_Dict[key]
+                    kwargs["Seen"][id(Dict[key])] = Dict[key]
                     Combine(Main_Dict[key], Dict[key], **kwargs)
             else:
                 if kwargs["Copy"].lower() == "deep":
