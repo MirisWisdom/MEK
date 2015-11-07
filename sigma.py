40187eefc1c90a6b6df3a0837a57fc73cad28366
Moses <none@none>
2015-11-06 22:38:14 -0500

    slightly more accurate(4x) arc length calculation

--- sigma.py
+++ sigma.py
@@ -14,6 +14,7 @@ A module for doing various calculus related things:
 '''
 
 import os
+from time import time
 
 from math import *
 from decimal import *
@@ -26,6 +27,8 @@ def alt(x):
 fact = factorial
 
 class sigma():
+
+    time = 0.0
     
     def __init__(self, y, n=10, a=0, b=1, **kwargs):
         self.n = n
@@ -39,7 +42,8 @@ class sigma():
     def eval(self, x):
         return round(self.y(x), self.precision)
 
-    def arc_length(self, n=None, a=None, b=None):
+    def arc_length_old(self, n=None, a=None, b=None):
+        #OUTDATED BY THE BELOW FUNCTION
         '''
         This function needs significant improvements.
         Takes around 15 seconds to calculate the arc of sqrt(1-x*x) at
@@ -47,6 +51,9 @@ class sigma():
         (spits out 3.14096039)
         '''
         self._sum = s = 0
+        self.time = 0.0
+        start = time()
+        
         if n is None: n = self.n
         if a is None: a = self.a
         if b is None: b = self.b
@@ -64,11 +71,60 @@ class sigma():
             xa = xb
         
         self._sum = s
+        self.time = time() - start
             
         return round(self._sum, self.precision)
 
+    def arc_length(self, n=None, a=None, b=None):
+        '''
+        A slightly more accurate method for calculating arc length than
+        the above method. Uses a simpsons rule style method for approximating
+        arc length. The exact method can be found at the link below.
+        
+        Normal Human (http://math.stackexchange.com/users/147263/normal-human)
+        Simpson's Rule like Method for Arc Length Approximation,
+        URL (version: 2014-05-02): http://math.stackexchange.com/q/778533
+        '''
+        self._sum = s = s_diff = 0
+        self.time = 0.0
+        start = time()
+        
+        if n is None: n = self.n
+        if a is None: a = self.a
+        if b is None: b = self.b
+        if a == b:    return 0
+        if n <= 0:    n = 1
+        y = self.y;   n = int(ceil(n/2))*2
+            
+        dx = (b-a)/n
+        dx_sq = dx*dx
+        y0 = y(a)
+        
+        for i in range(n):
+            y1 = y(a + dx*i)
+            s += sqrt(dx_sq + pow(y1-y0, 2) )
+            y0 = y1
+
+        dx = dx*2
+        dx_sq = dx*dx
+        y0 = y(a)
+        
+        for i in range(n//2):
+            y1 = y(a + dx*i)
+            s_diff += sqrt(dx_sq + pow(y1-y0, 2) )
+            y0 = y1
+        
+        self.time = time() - start
+        self._sum = round((4*s - s_diff)/3, self.precision)
+        
+        return self._sum
+        
+
     def mid(self, n=None, a=None, b=None):
         self._sum = s = 0
+        self.time = 0.0
+        start = time()
+        
         if n is None: n = self.n
         if a is None: a = self.a
         if b is None: b = self.b
@@ -90,10 +146,15 @@ class sigma():
             
             self._sum = s*dx
             
-        return round(self._sum, self.precision)
+        self.time = time() - start
+        self._sum = round(self._sum, self.precision)
+        return self._sum
 
     def left(self, n=None, a=None, b=None):
         self._sum = s = 0
+        self.time = 0.0
+        start = time()
+        
         if n is None: n = self.n
         if a is None: a = self.a
         if b is None: b = self.b
@@ -114,10 +175,15 @@ class sigma():
             
             self._sum = s*dx
             
-        return round(self._sum, self.precision)
+        self.time = time() - start
+        self._sum = round(self._sum, self.precision)
+        return self._sum
 
     def right(self, n=None, a=None, b=None):
         self._sum = s = 0
+        self.time = 0.0
+        start = time()
+        
         if n is None: n = self.n
         if a is None: a = self.a
         if b is None: b = self.b
@@ -138,10 +204,15 @@ class sigma():
             
             self._sum = s*dx
             
-        return round(self._sum, self.precision)
+        self.time = time() - start
+        self._sum = round(self._sum, self.precision)
+        return self._sum
 
     def trapezoid(self, n=None, a=None, b=None):
         self._sum = s = 0
+        self.time = 0.0
+        start = time()
+        
         if n is None: n = self.n
         if a is None: a = self.a
         if b is None: b = self.b
@@ -163,12 +234,16 @@ class sigma():
             
             s += (y(a) + y(b))/2
             s *= dx
-        
+            
+        self.time = time() - start
         self._sum = round(s, self.precision)
         return self._sum
 
     def simpson(self, n=None, a=None, b=None):
         self._sum = s = 0
+        self.time = 0.0
+        start = time()
+        
         if n is None: n = self.n
         if a is None: a = self.a
         if b is None: b = self.b
@@ -191,12 +266,14 @@ class sigma():
             s += y(a) + y(b) + 4*y(a+dx)
             s *= dx/3
         
+        self.time = time() - start
         self._sum = round(s, self.precision)
-        
         return self._sum
 
     def series_sum(self, n=None, a=None):
         self._sum = s = 0
+        self.time = 0.0
+        start = time()
         
         if n is None: n = self.n
         if a is None: a = self.a
@@ -210,12 +287,14 @@ class sigma():
             for i in range(int(a), int(n)):
                 s += y(i)
             
-            
-        self._sum = round(s, self.precision)
-        
+        self.time = time() - start
+        self._sum = round(self._sum, self.precision)
         return self._sum
 
     def sequence(self, a=None, b=None):
+        self.time = 0.0
+        start = time()
+        
         if a is None: a = self.a
         if b is None: b = self.b
         a, b = int(a), int(b)
@@ -229,6 +308,8 @@ class sigma():
         for i in range(a, b):
             sequence[i-a] = round(y(i), p)
             
+            
+        self.time = time() - start
         return sequence
 
     @property
@@ -263,6 +344,7 @@ if __name__ == '__main__':
                 "    'sum'  prints the last sum calculated.\n"+
                 "    'quit' exits the program.\n"+
                 "    'cls'  clears the window of all text.\n"+
+                "    'time' prints the amount of seconds the last computation took.\n"+
                 "    'help' prints this message.\n\n"+
                 "    '\\xxxx' compiles and executes xxxx as python code.\n"+
                 "         This is almost as if you were using the console.\n\n")
@@ -405,6 +487,8 @@ if __name__ == '__main__':
                     os.system('cls')
                 elif inp.lower() in ('quit', 'exit'):
                     raise SystemExit
+                elif inp.lower() == 'time':
+                    print('   ',calc.time)
                 elif inp[0].lower() == 'y':
                     try:
                         exec('Y = lambda x: '+inp.strip('yY= '))
